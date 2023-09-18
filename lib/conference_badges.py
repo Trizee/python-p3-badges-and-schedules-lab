1b5952211d19da004a2eab54b6fa98d70fa91c40
Tri <trihoang09@gmail.com>
2023-09-18 10:49:02 -0600

    completed lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,30 @@
 def badge_maker(name):
-    return None
+    return (f'Hello, my name is {name}.')
 
 def batch_badge_creator(names):
-    return None
+    l = []
+    for name in names:
+        l.append(f'Hello, my name is {name}.')
+    return l
 
 def assign_rooms(names):
-    return None
+    
+    l = []
+    for name in names:
+        l.append(f'Hello, {name}! You\'ll be assigned to room {names.index(name) + 1}!')
+    return l
+
+    
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    room_assignments = assign_rooms(names)
+
+    for badge in badges:
+        print(badge)
+    
+    for assignment in room_assignments:
+        print(assignment)
+
+
+printer(['1','2','3','4'])
